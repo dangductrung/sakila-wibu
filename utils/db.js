1b00ea7cc0babfe022fd488ffc7d29080eee2f9c
DerekZohar <derekzohar@gmail.com>
2020-11-14 21:00:51 +0700

    update db config

--- utils/db.js
+++ utils/db.js
@@ -1,11 +1,11 @@
 const knex = require('knex')({
   client: 'mysql2',
   connection: {
-    host: '127.0.0.1',
+    host: 'us-cdbr-east-02.cleardb.com',
     port: 3307,
-    user: 'root',
-    password: 'sakila',
-    database: 'sakila'
+    user: 'b5be0ab264a220',
+    password: 'd1245379',
+    database: 'sakila-wibu'
   }
 });
 
